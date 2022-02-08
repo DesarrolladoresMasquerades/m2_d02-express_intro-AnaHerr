fc93a2f7da8dbd023e3482b2a62953b9cae30de5
AnaHerr <ana.herrera.91@gmail.com>
2022-02-08 12:58:19 +0100

    1st commit exercise

--- exercise/app.js
+++ exercise/app.js
@@ -15,6 +15,24 @@ app.all("/home", (request, response)=>{//the request URL, what the user wants
      response.sendFile(__dirname + "/views/home.html")
 })
 
+app.all("/hobbies", (req, res)=>{
+    res.sendFile(__dirname + "/views/hobbies.html")
+})
+
+app.all("/hobbies/crochet", (req, res)=>{
+    res.sendFile(__dirname + "/views/crochet.html")
+})
+
+app.all("/hobbies/dancing", (req, res)=>{
+    res.sendFile(__dirname + "/views/dancing.html")
+})
+
+app.all("/hobbies/hiking", (req, res)=>{
+    res.sendFile(__dirname + "/views/hiking.html")
+})
+
+
 app.listen(3000, ()=>{
     console.log("App listening on port 3000")
 });
+
